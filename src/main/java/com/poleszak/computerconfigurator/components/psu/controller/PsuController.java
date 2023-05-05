6d97612ce1b25cbdde3088ade12494e60bf8dfa2
poleszak <poleszak@proton.me>
2023-05-05 19:53:47 +0200

    Implementation

--- src/main/java/com/poleszak/computerconfigurator/components/psu/controller/PsuController.java
+++ src/main/java/com/poleszak/computerconfigurator/components/psu/controller/PsuController.java
@@ -1,7 +1,11 @@
 package com.poleszak.computerconfigurator.components.psu.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PsuController {
+
+
 }
