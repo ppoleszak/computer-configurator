16902951ed6c6933c848cbb8054d2d554c61fbe1
piotrpoleszak <poleszak@protonmail.com>
2023-05-07 16:51:59 +0200

    Prepare computer configuration classes

--- src/main/java/com/poleszak/computerconfigurator/configurator/controller/ComputerConfigurationController.java
+++ src/main/java/com/poleszak/computerconfigurator/configurator/controller/ComputerConfigurationController.java
@@ -1,4 +1,9 @@
 package com.poleszak.computerconfigurator.configurator.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ComputerConfigurationController {
 }
